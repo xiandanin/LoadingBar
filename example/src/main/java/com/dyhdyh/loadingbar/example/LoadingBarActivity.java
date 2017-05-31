c40ac1a65b3f5796e2b28d04917ee9ec103fe8d6
dengyuhan <464958567@qq.com>
2017-05-31 10:25:49 +0800

    修改示例

--- example/src/main/java/com/dyhdyh/loadingbar/example/LoadingBarActivity.java
+++ example/src/main/java/com/dyhdyh/loadingbar/example/LoadingBarActivity.java
@@ -27,7 +27,7 @@ public class LoadingBarActivity extends BaseActivity {
 
     public void clickLoading(View v) {
         LoadingBar.make(mParent).show();
-        //LoadingBar.make(mParent).show(loadingView);
+        //LoadingBar.make(mParent,loadingView).show();
     }
 
 
